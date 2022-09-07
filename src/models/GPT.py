b555cf0b8bee71c4a3bd4066e103362a4fe497e1
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-07 11:47:29 -0400

    base structure for transformer done

--- src/models/GPT.py
+++ src/models/GPT.py
@@ -5,9 +5,11 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 from functools import partial
+from typing import Any, Callable
 
 
 class MLPBlock(nn.Module):
+    """Standard MLP Block"""
 
     embedding_dim: int
     dimension_multiplier: int = 4
@@ -15,9 +17,9 @@ class MLPBlock(nn.Module):
     N: int = None
 
     # Scale residual weights by 1/sqrt(N) where N is number of layers
-    residual_init = jax.nn.initializers.normal(stddev=0.02 / jnp.sqrt(N))
-    dense_init = jax.nn.initializers.normal(stddev=0.02)
-    bias_init = jax.nn.initializers.zeros()
+    residual_init: Callable = jax.nn.initializers.normal(stddev=0.02 / jnp.sqrt(N))
+    dense_init: Callable = jax.nn.initializers.normal(stddev=0.02)
+    bias_init: Callable = jax.nn.initializers.zeros()
 
     # TODO: How does mixed precision work?
 
@@ -41,6 +43,7 @@ class MLPBlock(nn.Module):
 
 
 class CausalAttention(nn.Module):
+    """Standard causal multi-headed attention"""
 
     embedding_dim: int
     num_head: int
@@ -49,9 +52,9 @@ class CausalAttention(nn.Module):
     N: int = None
 
     # Scale residual weights by 1/sqrt(N) where N is number of layers
-    residual_init = jax.nn.initializers.normal(stddev=0.02 / jnp.sqrt(N))
-    dense_init = jax.nn.initializers.normal(stddev=0.02)
-    bias_init = jax.nn.initializers.zeros()
+    residual_init: Callable = jax.nn.initializers.normal(stddev=0.02 / jnp.sqrt(N))
+    dense_init: Callable = jax.nn.initializers.normal(stddev=0.02)
+    bias_init: Callable = jax.nn.initializers.zeros()
 
     dtype = None
 
@@ -114,3 +117,93 @@ class CausalAttention(nn.Module):
         )(attn_out)
 
         return dropout(out)
+
+
+class TransformerBlock(nn.Module):
+    """One full transformer block"""
+
+    embedding_dim: int
+    num_head: int
+    block_size: int
+    residual_dropout: float = 0.0
+    N: int = None
+    dtype: Any = None
+    fused_residuals: bool = False
+
+    def setup(self):
+        self.attn = CausalAttention(
+            self.embedding_dim,
+            self.num_head,
+            self.block_size,
+            self.residual_dropout,
+            self.N,
+        )
+        self.mlp = MLPBlock(self.embedding_dim, dropout=self.residual_dropout, N=self.N)
+        self.ln1 = nn.LayerNorm()
+
+        if not self.fused_residuals:
+            self.ln2 = nn.LayerNorm()
+
+    def __call__(self, x: jnp.array, train: bool = False) -> jnp.array:
+
+        if self.fused_residuals:
+            return self.attn(self.ln1(x), train) + self.mlp(self.ln1(x), train)
+        else:
+            x = x + self.attn(self.ln1(x), train)
+            x = x + self.mlp(self.ln2(x), train)
+            return x
+
+
+class Transformer(nn.Module):
+    """
+    Full transformer
+    """
+
+    embedding_dim: int
+    vocab_size: int
+    num_head: int
+    block_size: int
+    residual_dropout: float = 0.0
+    N: int = None
+    dtype: Any = None
+    fused_residuals: bool = False
+
+    dense_init: Callable = jax.nn.initializers.normal(stddev=0.02)
+    bias_init: Callable = jax.nn.initializers.zeros()
+
+    @nn.compact
+    def __call__(self, x: jnp.array, train: bool = False) -> jnp.array:
+
+        B, T = x.size(0), x.size(1)
+
+        wte = nn.Embed(
+            name="wte",
+            num_embeddings=self.vocab_size,
+            features=self.embedding_dim,
+            embedding_init=self.dense_init,
+        )(x)
+        wpe = nn.Embed(
+            name="wpe",
+            num_embeddings=self.block_size,
+            features=self.embedding_dim,
+            embedding_init=self.dense_init,
+        )(jnp.ones(B, T))
+
+        x = wte + wpe
+
+        for _ in range(self.N):
+
+            x = TransformerBlock(
+                self.embedding_dim,
+                self.num_head,
+                self.block_size,
+                self.residual_dropout,
+                self.N,
+                self.fused_residuals,
+            )(x, train)
+
+        x = nn.LayerNorm(x)
+
+        logits = wte.attend(x)
+
+        return logits
