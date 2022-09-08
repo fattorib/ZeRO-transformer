c57963594be51261cc3aad41e9ebbf8a8d94c3d8
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-08 17:26:39 -0400

    model factory for loading from config file

--- src/models/GPT.py
+++ src/models/GPT.py
@@ -5,8 +5,9 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 from functools import partial
-from typing import Any, Callable
+from typing import Any
 import jax.nn.initializers as initializers
+from omegaconf import OmegaConf
 
 
 class MLPBlock(nn.Module):
@@ -20,19 +21,21 @@ class MLPBlock(nn.Module):
     # TODO: How does mixed precision work?
     @nn.compact
     def __call__(self, x: jnp.array, train: bool) -> jnp.array:
-        dropout = partial(nn.Dropout, rate=self.dropout, deterministic=not train)
+        dropout = partial(
+            nn.Dropout, rate=self.dropout, deterministic=not train
+        )
         x = nn.Dense(
             features=self.dimension_multiplier * self.embedding_dim,
             name="fc_in",
             kernel_init=initializers.normal(stddev=0.02),
-            bias_init=initializers.zeros
+            bias_init=initializers.zeros,
         )(x)
         x = nn.gelu(x)
         out = nn.Dense(
             features=self.embedding_dim,
             name="fc_residual",
             kernel_init=initializers.normal(stddev=0.02 / jnp.sqrt(self.N)),
-            bias_init=initializers.zeros
+            bias_init=initializers.zeros,
         )(x)
         return dropout()(out)
 
@@ -50,36 +53,50 @@ class CausalAttention(nn.Module):
 
     @nn.compact
     def __call__(self, x: jnp.array, train: bool) -> jnp.array:
-        dropout = partial(nn.Dropout, rate=self.dropout, deterministic=not train)
+        dropout = partial(
+            nn.Dropout, rate=self.dropout, deterministic=not train
+        )
 
         B, T, C = x.shape[:3]
 
         # Shape is (B, nh, T, h_dim)
-        key = nn.Dense(
+        key = (
+            nn.Dense(
                 name="key_proj",
                 features=self.embedding_dim,
                 kernel_init=initializers.normal(stddev=0.02),
-                bias_init=initializers.zeros
-            )(x).reshape(B, T, self.num_head, self.embedding_dim // self.num_head).transpose(0,2, 1,3)
-          
+                bias_init=initializers.zeros,
+            )(x)
+            .reshape(B, T, self.num_head, self.embedding_dim // self.num_head)
+            .transpose(0, 2, 1, 3)
+        )
+
         # Shape is (B, nh, T, h_dim)
-        value = nn.Dense(
+        value = (
+            nn.Dense(
                 name="value_proj",
                 features=self.embedding_dim,
                 kernel_init=initializers.normal(stddev=0.02),
-                bias_init=initializers.zeros
-            )(x).reshape(B, T, self.num_head, self.embedding_dim // self.num_head).transpose(0,2, 1,3)
+                bias_init=initializers.zeros,
+            )(x)
+            .reshape(B, T, self.num_head, self.embedding_dim // self.num_head)
+            .transpose(0, 2, 1, 3)
+        )
 
         # Shape is (B, nh, T, h_dim)
-        query = nn.Dense(
+        query = (
+            nn.Dense(
                 name="query_proj",
                 features=self.embedding_dim,
                 kernel_init=initializers.normal(stddev=0.02),
-                bias_init=initializers.zeros
-            )(x).reshape(B, T, self.num_head, self.embedding_dim // self.num_head).transpose(0,2, 1,3)
+                bias_init=initializers.zeros,
+            )(x)
+            .reshape(B, T, self.num_head, self.embedding_dim // self.num_head)
+            .transpose(0, 2, 1, 3)
+        )
 
         # get raw attention scores
-        attn_full = (query @ key.transpose(0,1, 3, 2)) / jnp.sqrt(
+        attn_full = (query @ key.transpose(0, 1, 3, 2)) / jnp.sqrt(
             key.shape[-1]
         )  # Shape is (B, nh, T, T)
 
@@ -87,14 +104,18 @@ class CausalAttention(nn.Module):
         masked_attn = jnp.where(mask, attn_full, jnp.finfo(self.dtype).min)
 
         attn_scores = nn.softmax(masked_attn, axis=-1)
-        attn_out = (attn_scores @ value).transpose(0,2, 1, 3)  # Shape is (B, T, nh, h_dim)
+        attn_out = (attn_scores @ value).transpose(
+            0, 2, 1, 3
+        )  # Shape is (B, T, nh, h_dim)
 
         attn_out = attn_out.reshape(B, T, C)
         out = nn.Dense(
             name="residual_out",
             features=self.embedding_dim,
-            kernel_init=jax.nn.initializers.normal(stddev=0.02 / jnp.sqrt(self.N)),
-            bias_init=initializers.zeros
+            kernel_init=jax.nn.initializers.normal(
+                stddev=0.02 / jnp.sqrt(self.N)
+            ),
+            bias_init=initializers.zeros,
         )(attn_out)
 
         return dropout()(out)
@@ -111,28 +132,33 @@ class TransformerBlock(nn.Module):
     dtype: Any = None
     fused_residuals: bool = False
 
-
     @nn.compact
     def __call__(self, x: jnp.array, train: bool = False) -> jnp.array:
 
         if self.fused_residuals:
             norm = nn.LayerNorm()
             return CausalAttention(
-            self.embedding_dim,
-            self.num_head,
-            self.block_size,
-            self.residual_dropout,
-            self.N,
-        )(norm(x), train) + MLPBlock(self.embedding_dim, dropout=self.residual_dropout, N=self.N)(norm(x), train)
+                self.embedding_dim,
+                self.num_head,
+                self.block_size,
+                self.residual_dropout,
+                self.N,
+            )(norm(x), train) + MLPBlock(
+                self.embedding_dim, dropout=self.residual_dropout, N=self.N
+            )(
+                norm(x), train
+            )
         else:
             x = x + CausalAttention(
-            self.embedding_dim,
-            self.num_head,
-            self.block_size,
-            self.residual_dropout,
-            self.N,
-        )(nn.LayerNorm()(x), train)
-            x = x + MLPBlock(self.embedding_dim, dropout=self.residual_dropout, N=self.N)(nn.LayerNorm()(x), train)
+                self.embedding_dim,
+                self.num_head,
+                self.block_size,
+                self.residual_dropout,
+                self.N,
+            )(nn.LayerNorm()(x), train)
+            x = x + MLPBlock(
+                self.embedding_dim, dropout=self.residual_dropout, N=self.N
+            )(nn.LayerNorm()(x), train)
             return x
 
 
@@ -159,7 +185,7 @@ class Transformer(nn.Module):
             name="wte",
             num_embeddings=self.vocab_size,
             features=self.embedding_dim,
-            embedding_init=initializers.normal(stddev=0.02)
+            embedding_init=initializers.normal(stddev=0.02),
         )
 
         wte = embed(x)
@@ -168,8 +194,8 @@ class Transformer(nn.Module):
             name="wpe",
             num_embeddings=self.block_size,
             features=self.embedding_dim,
-            embedding_init=initializers.normal(stddev=0.02)
-        )(jnp.ones((B, T), dtype = jnp.uint8))
+            embedding_init=initializers.normal(stddev=0.02),
+        )(jnp.ones((B, T), dtype=jnp.uint8))
 
         x = wte + wpe
 
@@ -189,3 +215,17 @@ class Transformer(nn.Module):
         logits = embed.attend(x)
 
         return logits
+
+
+def model_getter(model_size, config_path="conf/model_config.yaml") -> nn.Module:
+    """Loads model configuration from YAML files
+    and returns models
+
+    Args:
+        model_size (str): model name. This is checked against all top-level model names in the YAML file (defaults to 'conf/model_config.yaml')
+    """
+
+    configs = OmegaConf.load(config_path)
+    assert model_size in list(configs.keys()), "Invalid model name provided"
+
+    return Transformer(**configs.model_size)
--- tests/test_model_components.py
+++ tests/test_model_components.py
@@ -1,67 +1,99 @@
 import unittest
-from chex import assert_equal 
-import flax.linen as nn 
-import jax.numpy as jnp 
+from chex import assert_equal
+import flax.linen as nn
+import jax.numpy as jnp
 import jax.random as random
-from src.models.GPT import CausalAttention, MLPBlock, TransformerBlock, Transformer
+from src.models.GPT import (
+    CausalAttention,
+    MLPBlock,
+    TransformerBlock,
+    Transformer,
+)
+
 
 class TestMLP(unittest.TestCase):
-
     def setUp(self) -> None:
-        self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.init_rng, self.rng = random.split(random.PRNGKey(0))
         self.block_size = 512
 
     def tearDown(self) -> None:
-        pass 
+        pass
 
     def test_MLP_create(self):
 
-        mlp = MLPBlock(embedding_dim=128, dimension_multiplier=4, dropout=0.1, N = 10)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        mlp = MLPBlock(
+            embedding_dim=128, dimension_multiplier=4, dropout=0.1, N=10
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = mlp.init(self.init_rng, batch_cts, False)
 
     def test_MLP_fwd(self):
-        mlp = MLPBlock(embedding_dim=128, dimension_multiplier=4, dropout=0.1, N = 6)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        mlp = MLPBlock(
+            embedding_dim=128, dimension_multiplier=4, dropout=0.1, N=6
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = mlp.init(self.init_rng, batch_cts, False)
 
-        out = mlp.apply({'params':params['params']},batch_cts, train = True, rngs = {"dropout": self.rng})
+        out = mlp.apply(
+            {"params": params["params"]},
+            batch_cts,
+            train=True,
+            rngs={"dropout": self.rng},
+        )
         assert_equal(out.shape, batch_cts.shape)
 
-        out = mlp.apply({'params':params['params']},batch_cts, train = False, rngs = {"dropout": self.rng})
+        out = mlp.apply(
+            {"params": params["params"]},
+            batch_cts,
+            train=False,
+            rngs={"dropout": self.rng},
+        )
         assert_equal(out.shape, batch_cts.shape)
+
 
 class TestAttn(unittest.TestCase):
-
     def setUp(self) -> None:
-        self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.init_rng, self.rng = random.split(random.PRNGKey(0))
         self.block_size = 512
 
     def tearDown(self) -> None:
-        pass 
+        pass
 
     def test_attn_create(self):
 
-        attn = CausalAttention(embedding_dim=128, num_head = 8, block_size = 512, dropout=0.1, N = 6)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        attn = CausalAttention(
+            embedding_dim=128, num_head=8, block_size=512, dropout=0.1, N=6
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = attn.init(self.init_rng, batch_cts, False)
 
     def test_attn_fwd(self):
-        attn = CausalAttention(embedding_dim=128, num_head = 8, block_size = 512, dropout=0.1, N = 6)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        attn = CausalAttention(
+            embedding_dim=128, num_head=8, block_size=512, dropout=0.1, N=6
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = attn.init(self.init_rng, batch_cts, False)
 
-        out = attn.apply({'params':params['params']},batch_cts, train = True, rngs = {"dropout": self.rng})
+        out = attn.apply(
+            {"params": params["params"]},
+            batch_cts,
+            train=True,
+            rngs={"dropout": self.rng},
+        )
         assert_equal(out.shape, batch_cts.shape)
 
-        out = attn.apply({'params':params['params']},batch_cts, train = False, rngs = {"dropout": self.rng})
+        out = attn.apply(
+            {"params": params["params"]},
+            batch_cts,
+            train=False,
+            rngs={"dropout": self.rng},
+        )
         assert_equal(out.shape, batch_cts.shape)
 
 
 class TestTransformerBlock(unittest.TestCase):
-
     def setUp(self) -> None:
-        self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.init_rng, self.rng = random.split(random.PRNGKey(0))
         self.block_size = 512
 
     def tearDown(self) -> None:
@@ -69,40 +101,80 @@ class TestTransformerBlock(unittest.TestCase):
 
     def test_block_create_fused(self):
 
-        block = TransformerBlock(embedding_dim=128, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=True)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        block = TransformerBlock(
+            embedding_dim=128,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=True,
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = block.init(self.init_rng, batch_cts, False)
 
-        
     def test_block_fwd_fused(self):
 
-        block = TransformerBlock(embedding_dim=128, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=True)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        block = TransformerBlock(
+            embedding_dim=128,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=True,
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = block.init(self.init_rng, batch_cts, False)
-        
-        out = block.apply({'params':params['params']},batch_cts, train = True, rngs = {"dropout": self.rng})
+
+        out = block.apply(
+            {"params": params["params"]},
+            batch_cts,
+            train=True,
+            rngs={"dropout": self.rng},
+        )
         assert_equal(out.shape, batch_cts.shape)
 
     def test_block_create_standard(self):
 
-        block = TransformerBlock(embedding_dim=128, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=False)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        block = TransformerBlock(
+            embedding_dim=128,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=False,
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = block.init(self.init_rng, batch_cts, False)
 
     def test_block_fwd_standard(self):
 
-        block = TransformerBlock(embedding_dim=128, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=False)
-        batch_cts = random.normal(self.rng, shape = (1, 512, 128))
+        block = TransformerBlock(
+            embedding_dim=128,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=False,
+        )
+        batch_cts = random.normal(self.rng, shape=(1, 512, 128))
         params = block.init(self.init_rng, batch_cts, False)
 
-        out = block.apply({'params':params['params']},batch_cts, train = True, rngs = {"dropout": self.rng})
+        out = block.apply(
+            {"params": params["params"]},
+            batch_cts,
+            train=True,
+            rngs={"dropout": self.rng},
+        )
         assert_equal(out.shape, batch_cts.shape)
 
 
 class TestGPT(unittest.TestCase):
-
     def setUp(self) -> None:
-        self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.init_rng, self.rng = random.split(random.PRNGKey(0))
         self.vocab_size = 256
         self.block_size = 512
 
@@ -111,30 +183,84 @@ class TestGPT(unittest.TestCase):
 
     def test_gpt_create_fused(self):
 
-        block = Transformer(embedding_dim=128, vocab_size = 256, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=True)
-        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        block = Transformer(
+            embedding_dim=128,
+            vocab_size=256,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=True,
+        )
+        batch_tok = random.randint(
+            self.rng, shape=(1, 512), maxval=256, minval=0
+        )
         params = block.init(self.init_rng, batch_tok, False)
-    
+
     def test_gpt_fwd_fused(self):
 
-        block = Transformer(embedding_dim=128, vocab_size = self.vocab_size, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=True)
-        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        block = Transformer(
+            embedding_dim=128,
+            vocab_size=self.vocab_size,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=True,
+        )
+        batch_tok = random.randint(
+            self.rng, shape=(1, 512), maxval=256, minval=0
+        )
         params = block.init(self.init_rng, batch_tok, False)
 
-        out = block.apply({'params':params['params']},batch_tok, train = True, rngs = {"dropout": self.rng})
-        assert_equal((1,self.block_size,self.vocab_size), out.shape)
+        out = block.apply(
+            {"params": params["params"]},
+            batch_tok,
+            train=True,
+            rngs={"dropout": self.rng},
+        )
+        assert_equal((1, self.block_size, self.vocab_size), out.shape)
 
     def test_gpt_create_standard(self):
 
-        block = Transformer(embedding_dim=128, vocab_size = 256, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=False)
-        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        block = Transformer(
+            embedding_dim=128,
+            vocab_size=256,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=False,
+        )
+        batch_tok = random.randint(
+            self.rng, shape=(1, 512), maxval=256, minval=0
+        )
         params = block.init(self.init_rng, batch_tok, False)
-    
+
     def test_gpt_fwd_standard(self):
 
-        block = Transformer(embedding_dim=128, vocab_size = self.vocab_size, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=False)
-        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        block = Transformer(
+            embedding_dim=128,
+            vocab_size=self.vocab_size,
+            num_head=8,
+            block_size=512,
+            residual_dropout=0.1,
+            N=6,
+            dtype=None,
+            fused_residuals=False,
+        )
+        batch_tok = random.randint(
+            self.rng, shape=(1, 512), maxval=256, minval=0
+        )
         params = block.init(self.init_rng, batch_tok, False)
 
-        out = block.apply({'params':params['params']},batch_tok, train = True, rngs = {"dropout": self.rng})
-        assert_equal((1,self.block_size,self.vocab_size), out.shape)
+        out = block.apply(
+            {"params": params["params"]},
+            batch_tok,
+            train=True,
+            rngs={"dropout": self.rng},
+        )
+        assert_equal((1, self.block_size, self.vocab_size), out.shape)
