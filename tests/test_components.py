2f3f141ad4c6c27e35c8a84d978f08a53814a65d
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-08 17:18:26 -0400

    added final tests, everything is passing

--- tests/test_components.py
+++ tests/test_components.py
@@ -9,6 +9,7 @@ class TestMLP(unittest.TestCase):
 
     def setUp(self) -> None:
         self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.block_size = 512
 
     def tearDown(self) -> None:
         pass 
@@ -34,6 +35,7 @@ class TestAttn(unittest.TestCase):
 
     def setUp(self) -> None:
         self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.block_size = 512
 
     def tearDown(self) -> None:
         pass 
@@ -60,6 +62,7 @@ class TestTransformerBlock(unittest.TestCase):
 
     def setUp(self) -> None:
         self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.block_size = 512
 
     def tearDown(self) -> None:
         pass
@@ -100,6 +103,8 @@ class TestGPT(unittest.TestCase):
 
     def setUp(self) -> None:
         self.init_rng, self.rng = random.split(random.PRNGKey(0)) 
+        self.vocab_size = 256
+        self.block_size = 512
 
     def tearDown(self) -> None:
         pass
@@ -107,5 +112,29 @@ class TestGPT(unittest.TestCase):
     def test_gpt_create_fused(self):
 
         block = Transformer(embedding_dim=128, vocab_size = 256, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=True)
-        batch_cts = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
-        params = block.init(self.init_rng, batch_cts, False)
+        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        params = block.init(self.init_rng, batch_tok, False)
+    
+    def test_gpt_fwd_fused(self):
+
+        block = Transformer(embedding_dim=128, vocab_size = self.vocab_size, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=True)
+        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        params = block.init(self.init_rng, batch_tok, False)
+
+        out = block.apply({'params':params['params']},batch_tok, train = True, rngs = {"dropout": self.rng})
+        assert_equal((1,self.block_size,self.vocab_size), out.shape)
+
+    def test_gpt_create_standard(self):
+
+        block = Transformer(embedding_dim=128, vocab_size = 256, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=False)
+        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        params = block.init(self.init_rng, batch_tok, False)
+    
+    def test_gpt_fwd_standard(self):
+
+        block = Transformer(embedding_dim=128, vocab_size = self.vocab_size, num_head = 8, block_size = 512, residual_dropout=0.1, N = 6, dtype = None, fused_residuals=False)
+        batch_tok = random.randint(self.rng, shape = (1, 512),maxval=256, minval = 0)
+        params = block.init(self.init_rng, batch_tok, False)
+
+        out = block.apply({'params':params['params']},batch_tok, train = True, rngs = {"dropout": self.rng})
+        assert_equal((1,self.block_size,self.vocab_size), out.shape)
