032b98285482719ec3ae9e819549720f479b513b
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-10 11:25:01 -0400

    update for preparation on shell

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,5 @@
+pip install --upgrade pip
+pip install -r requirements
 cp keys.py src/data_utils/keys.py
 python src/data_utils/download_dataset.py
 tar -xvf  'data/processed/bookcorpus_shards.tar.gz' -C 'data/processed'
