0d57e8cec356f95c04cdafcc005f134f7d1880dc
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-11 18:20:25 -0400

    prepare options for TPU on colab

---
+++ prepareTPU.sh
@@ -0,0 +1,6 @@
+pip install --upgrade pip
+pip install "jax[cuda11_cudnn805]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install -r requirements
+cp keys.py src/data_utils/keys.py
+python src/data_utils/download_dataset.py
+tar -xvf  'data/processed/bookcorpus_shards.tar.gz' -C 'data/processed'
