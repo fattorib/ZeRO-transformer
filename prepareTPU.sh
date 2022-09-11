4da833304855ba6302b6d08d18a47ce15caada10
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-11 18:31:05 -0400

    remove jax cudnn

--- prepareTPU.sh
+++ prepareTPU.sh
@@ -1,5 +1,4 @@
 pip install --upgrade pip
-pip install "jax[cuda11_cudnn805]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -r requirements
 cp keys.py src/data_utils/keys.py
 python src/data_utils/download_dataset.py
