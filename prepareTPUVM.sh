7f9dc7107e2e373b5f7d254ffe5db0d207449e89
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-14 10:49:44 -0400

    updating shell for TPU VM

--- prepareTPUVM.sh
+++ prepareTPUVM.sh
@@ -1,4 +1,5 @@
 pip install --upgrade pip
+pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r requirements
 cp keys.py src/data_utils/keys.py
 python src/data_utils/download_dataset.py
