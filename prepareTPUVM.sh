c4d74b31aabdc084ec1bf0cdc9c4acca6f2c5731
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-15 11:00:15 -0400

    updates to bash, configs

--- prepareTPUVM.sh
+++ prepareTPUVM.sh
@@ -1,6 +1,3 @@
 pip install --upgrade pip
 pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r requirements
-cp keys.py src/data_utils/keys.py
-python src/data_utils/download_dataset.py
-tar -xvf  'data/processed/bookcorpus_shards.tar.gz' -C 'data/processed'
