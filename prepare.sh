82515ce9d47a31f37e8495f1f4ffb8286fff9ebf
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-18 12:25:53 -0400

    download for books2

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,5 @@ pip install --upgrade pip
 pip install "jax[cuda11_cudnn805]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -r requirements
 cp keys.py src/data_utils/keys.py
-python src/data_utils/download_dataset.py
+python src/data_utils/download_dataset_books2.py
 tar -xvf  'data/processed/bookcorpus_shards.tar.gz' -C 'data/processed'
