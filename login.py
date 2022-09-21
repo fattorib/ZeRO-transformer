f6e79aa02849aa72afdb7130b395196cb391a196
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-21 18:37:44 -0400

    change to login

--- login.py
+++ login.py
@@ -1,4 +1,16 @@
 import wandb
+import argparse
+
+def parse():
+    parser = argparse.ArgumentParser(description="Transformer Training")
+
+    parser.add_argument(
+        "--key",
+    )
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
-    wandb.login()
+    args = parse()
+    wandb.login(key = args.key)
