dc561cae3b55e54ccd6fc4c9bf51995c0117f8ac
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-22 09:54:08 -0400

    uncommenting

--- prepareTPUVM.sh
+++ prepareTPUVM.sh
@@ -1,3 +1,3 @@
 pip install --upgrade pip
 pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-# pip install -r requirements
+pip install -r requirements
