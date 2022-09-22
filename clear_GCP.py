c5bb890c30f226fa511c2d2ff09178c6c3801877
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-22 14:19:12 -0400

    try this

--- clear_GCP.py
+++ clear_GCP.py
@@ -3,8 +3,11 @@ from google.cloud.exceptions import NotFound
 
 client = storage.Client()
 
-bucket = client.get_bucket(f"gs://bfattoriwebtext2")
-print(bucket)
-blobs = bucket.list_blobs(prefix=f"checkpoints")
-for blob in blobs:
-    blob.delete()
+# bucket = client.get_bucket(f"gs://bfattoriwebtext2")
+# print(bucket)
+# blobs = bucket.list_blobs(prefix=f"checkpoints")
+# for blob in blobs:
+#     blob.delete()
+
+for blob in client.list_blobs(bucket = f"gs://bfattoriwebtext2", prefix=f"checkpoints/"):
+    print(blob)
