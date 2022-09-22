ba8ebfef0b97d0248a3f23401c23b3e7b5075c95
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-22 10:18:00 -0400

    wrap in try/except to avoid crash?

--- main_TPUVM.py
+++ main_TPUVM.py
@@ -454,4 +454,8 @@ def eval_step(state: Any, batch: jnp.array):
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f'Error encountered: {e}')
+
