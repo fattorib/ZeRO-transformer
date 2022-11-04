a7729413c0545f65ed0cf957ac47bbd65c0a1335
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2022-11-04 23:34:13 +0000

    adding back remaining dropped code

--- main.py
+++ main.py
@@ -488,48 +488,48 @@ def main():
                     cfg.training.evaluation_frequency
                     * cfg.training.gradient_accumulation_steps
                 ) == 0:
-                #     for val_it, val_text in enumerate(
-                #         tqdm(vl, disable=not jax.process_index() == 0)
-                #     ):
-                #         if val_it < cfg.training.maximum_evaluation_steps:
-                #             # sharded_batch = shard(val_text)
-                #             metrics = pjit_eval_step(state, val_text)
-                #             validation_metrics.append(metrics)
-                #         else:
-                #             break
+                    for val_it, val_text in enumerate(
+                        tqdm(vl, disable=not jax.process_index() == 0)
+                    ):
+                        if val_it < cfg.training.maximum_evaluation_steps:
+                            # sharded_batch = shard(val_text)
+                            metrics = pjit_eval_step(state, val_text)
+                            validation_metrics.append(metrics)
+                        else:
+                            break
 
-                #     validation_metrics_np = {
-                #         k: np.mean([metrics[k] for metrics in validation_metrics])
-                #         for k in validation_metrics[0]
-                #     }
+                    validation_metrics_np = {
+                        k: np.mean([metrics[k] for metrics in validation_metrics])
+                        for k in validation_metrics[0]
+                    }
 
-                #     if jax.process_index() == 0:
-                #         train_metrics_np.update(validation_metrics_np)
-                #         train_metrics_np.pop("Train Batch Time")
-                #         wandb.log(train_metrics_np)
+                    if jax.process_index() == 0:
+                        train_metrics_np.update(validation_metrics_np)
+                        train_metrics_np.pop("Train Batch Time")
+                        wandb.log(train_metrics_np)
 
-                #         if cfg.device.mp_devices > 1:
-                #             device_state = jax.device_get(
-                #                 state
-                #             )  # pull a copy of the sharded state to CPU and save
-                #             save_checkpoint(
-                #                 device_state,
-                #                 workdir=f"gs://{cfg.data.bucket_path}/{cfg.data.checkpoint_directory}",
-                #                 bucket_path=bucket_path,
-                #                 client=client,
-                #             )
+                        if cfg.device.mp_devices > 1:
+                            device_state = jax.device_get(
+                                state
+                            )  # pull a copy of the sharded state to CPU and save
+                            save_checkpoint(
+                                device_state,
+                                workdir=f"gs://{cfg.data.bucket_path}/{cfg.data.checkpoint_directory}",
+                                bucket_path=bucket_path,
+                                client=client,
+                            )
 
-                #         else:
-                #             if save_to_bucket:
-                #                 save_checkpoint(
-                #                     state,
-                #                     workdir=f"gs://{cfg.data.bucket_path}/{cfg.data.checkpoint_directory}",
-                #                 )
-                #             else:
-                #                 save_checkpoint(
-                #                     state, workdir=cfg.data.checkpoint_directory
-                #                 )
-                    pass 
+                        else:
+                            if save_to_bucket:
+                                save_checkpoint(
+                                    state,
+                                    workdir=f"gs://{cfg.data.bucket_path}/{cfg.data.checkpoint_directory}",
+                                )
+                            else:
+                                save_checkpoint(
+                                    state, workdir=cfg.data.checkpoint_directory
+                                )
+                    # pass 
 
                 else:
                     if jax.process_index() == 0:
