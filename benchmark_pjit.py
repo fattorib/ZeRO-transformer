afa81ab56c6c2ad5f122903332688f3f4a74bcb7
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2022-11-19 19:00:14 +0000

    mesh change

--- benchmark_pjit.py
+++ benchmark_pjit.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     NUM_PASSES = 10
 
     # Setting up device mesh
-    mesh_shape = (4, 2)
+    mesh_shape = (1, 8)
     devices = np.asarray(jax.devices()).reshape(*mesh_shape)
     mesh = Mesh(devices, ("dp", "mp"))
 
