624404bfdfcef85ece08e37d2068410cdf9d7763
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2022-12-24 12:36:34 -0500

    copy to CPU during restore

--- main_zero.py
+++ main_zero.py
@@ -113,9 +113,13 @@ def restore_opt_checkpoint(workdir: str) -> Tuple[Any, int]:
     mu_pytree = jax.tree_util.tree_map(
         lambda x: jnp.array(x), opt_state_restored["opt_state"]["1"]["0"]["mu"]
     )
+    mu_pytree = jax.device_get(mu_pytree)
+
     nu_pytree = jax.tree_util.tree_map(
         lambda x: jnp.array(x), opt_state_restored["opt_state"]["1"]["0"]["nu"]
     )
+    nu_pytree = jax.device_get(nu_pytree)
+    
     count_pytree = jax.tree_util.tree_map(
         lambda x: jnp.array(x), opt_state_restored["opt_state"]["1"]["0"]["count"]
     )
