7d512a88908b1c60f950bfa8694dea872a850cd4
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-01-26 20:24:33 +0000

    weight_decay read from config.yaml

--- main_zero.py
+++ main_zero.py
@@ -266,7 +266,7 @@ def main():
     state, optimizer, opt_state = create_zero_train_state(
         init_rng,
         learning_rate_fn,
-        weight_decay=0.1,
+        weight_decay=cfg.training.weight_decay,
         model=model,
     )
     params = state.params
