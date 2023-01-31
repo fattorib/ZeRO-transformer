ccb3d30536606cd352d2227894a4e1ab89f28589
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-01-31 21:59:56 +0000

    smaller validation batches

--- main_zero.py
+++ main_zero.py
@@ -408,7 +408,7 @@ def main():
 
     vl = DataLoader(
         dataset=validation_dataset,
-        batch_size=cfg.training.batch_size,
+        batch_size=cfg.training.batch_size//2,
         collate_fn=numpy_collate,
         drop_last=True,
     )
