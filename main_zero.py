17dfd35dc86430a681df5a51bfd0e51cd9ceaa62
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-02-01 14:36:54 +0000

    use current ctx during eval

--- main_zero.py
+++ main_zero.py
@@ -520,7 +520,7 @@ def main():
             for val_it, val_text in enumerate(
                 tqdm(vl, disable=not jax.process_index() == 0)
             ):
-                val_text = val_text[:, : cfg.training.warmup_train_context]
+                val_text = val_text[:, : seq_len]
                 val_text = shard(val_text)
 
                 if val_it < cfg.training.maximum_evaluation_steps:
