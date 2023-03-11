56f07ab39f5dac5c1bc96b543bd30a62417db6be
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-03-11 22:32:46 +0000

    pin newer jax version

--- prepareTPUVM.sh
+++ prepareTPUVM.sh
@@ -1,3 +1,3 @@
 pip install --upgrade pip
-pip install "jax[tpu]==0.4.1" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r requirements
