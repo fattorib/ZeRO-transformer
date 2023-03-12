95f32e17e9f0f0bdc7efef3f96849cd1d9dc073a
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-03-12 17:37:25 +0000

    update pytorch conversion code to optionally include biases

--- torch_compatability/flax_to_pytorch.py
+++ torch_compatability/flax_to_pytorch.py
@@ -3,30 +3,36 @@ import torch
 from flax.serialization import msgpack_restore
 
 
-def create_transformer_block_mapping(block_idx: int):
+def create_transformer_block_mapping(block_idx: int, use_bias: bool = False):
     """
     Creates required flax -> PyTorch mapping for a specific transformer block
     """
-
-    return {
+    dict_params = {
         "CausalAttention_0.key_proj.kernel": f"blocks.{block_idx}.attn.key.weight",
         "CausalAttention_0.value_proj.kernel": f"blocks.{block_idx}.attn.value.weight",
         "CausalAttention_0.query_proj.kernel": f"blocks.{block_idx}.attn.query.weight",
+        "CausalAttention_0.residual_out.kernel": f"blocks.{block_idx}.attn.fc_resid.weight",
+        "MLPBlock_0.fc_in.kernel": f"blocks.{block_idx}.mlp.fc1.weight",
+        "MLPBlock_0.fc_residual.kernel": f"blocks.{block_idx}.mlp.fc_resid.weight",
+        "LayerNorm_0.scale": f"blocks.{block_idx}.ln1.weight",
+        "LayerNorm_1.scale": f"blocks.{block_idx}.ln2.weight",
+        "MLPBlock_0.fc_in.bias": f"blocks.{block_idx}.mlp.fc1.bias",
+    }
+
+    if use_bias:
+        bias_dict = {
+        "CausalAttention_0.residual_out.bias": f"blocks.{block_idx}.attn.fc_resid.bias",
+        "MLPBlock_0.fc_residual.bias": f"blocks.{block_idx}.mlp.fc_resid.bias",
         "CausalAttention_0.key_proj.bias": f"blocks.{block_idx}.attn.key.bias",
         "CausalAttention_0.value_proj.bias": f"blocks.{block_idx}.attn.value.bias",
         "CausalAttention_0.query_proj.bias": f"blocks.{block_idx}.attn.query.bias",
-        "CausalAttention_0.residual_out.kernel": f"blocks.{block_idx}.attn.fc_resid.weight",
-        "CausalAttention_0.residual_out.bias": f"blocks.{block_idx}.attn.fc_resid.bias",
-        "MLPBlock_0.fc_in.kernel": f"blocks.{block_idx}.mlp.fc1.weight",
-        "MLPBlock_0.fc_in.bias": f"blocks.{block_idx}.mlp.fc1.bias",
-        "MLPBlock_0.fc_residual.kernel": f"blocks.{block_idx}.mlp.fc_resid.weight",
-        "MLPBlock_0.fc_residual.bias": f"blocks.{block_idx}.mlp.fc_resid.bias",
-        "LayerNorm_0.scale": f"blocks.{block_idx}.ln1.weight",
         "LayerNorm_0.bias": f"blocks.{block_idx}.ln1.bias",
-        "LayerNorm_1.scale": f"blocks.{block_idx}.ln2.weight",
         "LayerNorm_1.bias": f"blocks.{block_idx}.ln2.bias",
-    }
+        }
 
+        dict_params.update(bias_dict)
+
+    return dict_params
 
 def flatten(p, label=None):
     if isinstance(p, dict):
@@ -36,7 +42,7 @@ def flatten(p, label=None):
         yield (label, p)
 
 
-def match_transformer_block(pytree, state_dict, block_idx):
+def match_transformer_block(pytree, state_dict, block_idx, use_bias):
     """
     Performs matching at an individual block level.
 
@@ -44,7 +50,7 @@ def match_transformer_block(pytree, state_dict, block_idx):
     the required block
     """
 
-    block_mappings = create_transformer_block_mapping(block_idx)
+    block_mappings = create_transformer_block_mapping(block_idx, use_bias)
 
     flattened_block = dict(flatten(pytree["params"][f"TransformerBlock_{block_idx}"]))
 
@@ -60,7 +66,7 @@ def match_transformer_block(pytree, state_dict, block_idx):
     return state_dict
 
 
-def match_and_save(model: torch.nn.Module, flax_save_path: str, out_save_path: str):
+def match_and_save(model: torch.nn.Module, flax_save_path: str, out_save_path: str, use_bias: bool = False):
     """
     Top-level function which performs matching for all blocks, including wte/LN
 
@@ -79,15 +85,17 @@ def match_and_save(model: torch.nn.Module, flax_save_path: str, out_save_path: s
     state_dict = model.state_dict()
 
     for block_idx in range(model.N):
-        state_dict = match_transformer_block(pytree, state_dict, block_idx)
+        state_dict = match_transformer_block(pytree, state_dict, block_idx, use_bias)
 
     # Manually set top-layer weights
     state_dict["norm.weight"] = torch.from_numpy(
         np.array(pytree["params"]["LayerNorm_0"]["scale"])
     )
-    state_dict["norm.bias"] = torch.from_numpy(
-        np.array(pytree["params"]["LayerNorm_0"]["bias"])
-    )
+
+    if use_bias: 
+        state_dict["norm.bias"] = torch.from_numpy(
+            np.array(pytree["params"]["LayerNorm_0"]["bias"])
+        )
     state_dict["wte.weight"] = torch.from_numpy(
         np.array(pytree["params"]["wte"]["embedding"])[
             : model.vocab_size,
