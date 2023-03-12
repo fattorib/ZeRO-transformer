f9252939c19977240068c1d9d63c49569c04fd3e
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-03-12 00:16:01 +0000

    debug to_bf16, disabling for now

--- src/partitioning/xmap_train_functions.py
+++ src/partitioning/xmap_train_functions.py
@@ -9,10 +9,12 @@ import optax
 from jax.lax import with_sharding_constraint
 
 def to_bf16(t):
-    return jax.tree_map(lambda x: x.astype(jnp.bfloat16) if x.dtype == jnp.float32 else x,t)
+    # return jax.tree_map(lambda x: x.astype(jnp.bfloat16) if x.dtype == jnp.float32 else x,t)
+    return t
 
 def to_f32(t):
-    return jax.tree_map(lambda x: x.astype(jnp.float32) if x.dtype == jnp.bfloat16 else x, t)
+    # return jax.tree_map(lambda x: x.astype(jnp.float32) if x.dtype == jnp.bfloat16 else x, t)
+    return t 
 
 
 # we xmap this
