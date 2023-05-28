03bee977470b0978bf1a014987a1af14895668e9
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-05-28 19:23:24 +0100

    bump jax version on TPU

--- prepareTPUVM.sh
+++ prepareTPUVM.sh
@@ -1,3 +1,3 @@
 pip install --upgrade pip
-pip install "jax[tpu]==0.4.8" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install "jax[tpu]==0.4.10" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r requirements
