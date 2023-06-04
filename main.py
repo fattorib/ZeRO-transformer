b6dcd39297804c4fd856b82cf1f2bb3963cbc607
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-06-04 11:20:45 +0100

    update main script

---
+++ main.py
@@ -0,0 +1,472 @@
+import argparse
+import logging
+import random as pyrandom
+from functools import partial
+from typing import Any, Callable, Tuple, Union
+
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+import torch
+import webdataset as wds
+from flax.training import checkpoints, train_state
+from jax.experimental.pjit import pjit
+from jax.experimental.shard_map import shard_map
+from jax.sharding import Mesh, PartitionSpec as P
+from omegaconf import OmegaConf
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from time import time 
+
+import wandb
+from src.models.GPT import model_getter, Transformer
+from src.partitioning.partition import create_opt_spec
+from training.train_functions import (eval_step, train_step,
+                                                   update_opt_state)
+from src.training.training_utils import compute_tokens_seen
+from src.utils.configs import flatten_dict
+from src.utils.dataloader import numpy_collate
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="Transformer Training")
+
+    parser.add_argument("--cfg", default="conf/config.yaml", type=str)
+
+    parser.add_argument("--model-cfg", default="conf/model_config.yaml", type=str)
+
+    parser.add_argument(
+        "--resume",
+        default=False,
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def save_checkpoint(params: Any, opt_state: Any, step: int, workdir: str) -> None:
+    """
+    Save a copy of params.
+
+    TODO: Add async manager to do this in a background process
+    """
+    if jax.process_index() == 0:
+        params = jax.device_get(params)
+        opt_state = jax.device_get(opt_state)
+        faux_state = train_state.TrainState(
+            step=step, apply_fn=None, params=params, tx=None, opt_state=opt_state
+        )
+        checkpoints.save_checkpoint(
+            workdir, faux_state, step, keep=5, overwrite=True, prefix="state_"
+        )
+
+
+
+def restore_checkpoint(params: Any, opt_state: Any, workdir: str) -> Tuple[Any, Any, int]:
+    """
+    Restores the most recent parameter dict using existing params and opt_state as targets
+    to rebuild
+    """
+    target = train_state.TrainState(
+            step = 0,
+            apply_fn=None, 
+            tx = None,
+            params = params,
+            opt_state=opt_state
+        )
+
+    restored = checkpoints.restore_checkpoint(workdir, target=target, prefix="state_")
+    params = restored.params
+    opt_state = restored.opt_state
+    step = restored.step
+    return params, opt_state, step
+
+
+def create_train_state(
+    rng: jax.random.PRNGKey,
+    learning_rate_fn: Union[float, Callable],
+    weight_decay: float,
+    model: nn.Module,
+) -> Tuple[Any, optax.GradientTransformation]:
+    """
+    Gets the abstract model shapes and sets up the optimizer.
+    """
+
+    batch = jax.random.randint(rng, shape=(1, model.block_size), maxval=50257, minval=0)
+    param_abstract = jax.eval_shape(
+        model.init, rng, batch
+    )
+
+    # This mask turns off weight decay for bias terms, LN terms and position embeddings
+    mask = jax.tree_map(
+        lambda x: x.ndim != 1 and x.shape != (model.block_size, model.embedding_dim),
+        param_abstract,
+    )
+
+    tx = optax.chain(
+        optax.clip(1.0),
+        optax.lion(
+            learning_rate=learning_rate_fn,
+            weight_decay=weight_decay,
+            mask=mask,
+        ),
+    )
+
+    return param_abstract, tx
+
+
+def main():
+    args = parse()
+    cfg = OmegaConf.load(args.cfg)
+
+    # getting system information
+    num_devices = jax.device_count()
+    num_local_devices = jax.local_device_count()
+    num_host = num_devices // num_local_devices
+    platform = jax.local_devices()[0].platform
+
+    mesh = Mesh(np.array(jax.devices()).reshape(cfg.training.dp, cfg.training.mp), ("dp", "mp"))
+
+    # setting up GCP bucket/client info if training on TPU
+    save_to_bucket = False
+    client = None
+    if platform == "tpu":
+        if cfg.data.bucket_path is not None:
+            # use GCP
+            from google.cloud import storage
+            from google.cloud.exceptions import NotFound
+
+            client = storage.Client()
+            save_to_bucket = True
+            train_shards = open(cfg.data.index_path_train).read().splitlines()
+            validation_shards = open(cfg.data.index_path_validation).read().splitlines()
+
+    else:
+        raise NotImplementedError("Training not currently supported on GPU.")
+
+    model_full, model_config = model_getter(
+        cfg.model.size, config_path=args.model_cfg, return_cfg=True, dtype=jnp.float32
+    )
+
+    # set up sharded config and model too
+    model_config["num_shard"] = mesh.shape["mp"]
+    model_config["tp_comms"] = True if mesh.shape["mp"] > 1 else False
+    model_shard = Transformer(*model_config)
+
+    learning_rate_fn = optax.warmup_cosine_decay_schedule(
+        init_value=0,
+        peak_value=cfg.training.peak_learning_rate,
+        warmup_steps=cfg.training.warmup_steps,
+        decay_steps=143000,
+        end_value=cfg.training.end_learning_rate,
+    )
+
+    rng = jax.random.PRNGKey(0)
+    rng, init_rng = jax.random.split(rng)
+
+    resume_step = 0
+
+    param_abstract, tx = create_train_state(
+        init_rng,
+        learning_rate_fn,
+        weight_decay=cfg.training.weight_decay,
+        model=model_full,
+    )
+    
+    # Setup partition specs
+
+    param_spec = nn.get_partition_spec(param_abstract)
+    grad_spec = param_spec
+    batch_spec = P("dp", None)
+    no_shard = P(None)
+    
+    opt_state_shapes = jax.eval_shape(tx.init, params)
+    opt_state_spec = create_opt_spec(param_spec, opt_state_shapes)
+
+    # Setup params and optimizer states
+    with mesh:
+        # do actual layer init wrapping with pjit
+        batch = jnp.ones((1, model_full.block_size), dtype=jnp.int32)
+        params = pjit(model_full.init, out_axis_resources=param_spec)(rng, batch)
+
+        opt_state = pjit(
+            tx.init,
+            in_axis_resources=(param_spec,),
+            out_axis_resources=opt_state_spec,
+        )(params)
+
+    train_step_tp = jax.jit(
+        shard_map(
+            partial(train_step, model=model_shard, accum_steps=cfg.training.gradient_accumulation_steps),
+            in_specs=(param_spec, batch_spec),
+            out_specs=(grad_spec, P(None)),
+            mesh=mesh,
+            check_rep=False,
+        )
+    )
+
+    eval_step_tp = jax.jit(
+        shard_map(
+            partial(eval_step, model=model_shard),
+            in_specs=(param_spec, batch_spec),
+            out_specs=(P(None)),
+            mesh=mesh,
+            check_rep=False,
+        )
+    )
+
+    with mesh:
+        update_opt_step_tp = pjit(
+            partial(update_opt_state, optimizer=tx, tp_spec=grad_spec),
+            in_axis_resources=(param_spec, grad_spec, opt_state_spec),
+            out_axis_resources=(param_spec, opt_state_spec),
+            donate_argnums=0,
+        )
+
+
+    if args.resume:
+        del params
+
+        if save_to_bucket:
+            
+            params, opt_state, step = restore_checkpoint(params, opt_state, workdir=f"gs://{cfg.data.bucket_path}/{cfg.data.checkpoint_directory}/")
+            resume_step = int(step)
+            
+            import gc 
+            gc.collect()
+
+        else:
+            raise NotImplementedError(
+                "Checkpointing not currently implemented for GPU."
+            )
+
+        if jax.process_index() == 0:
+            logger.debug(f"Resuming training from step {resume_step}")
+
+
+    if jax.process_index() == 0:
+        logger.debug(f"VM setup with {num_devices} devices.")
+        logger.debug(f"Host setup with {num_local_devices} devices.")
+        logger.debug(f"Using platform: {platform}.")
+        logger.debug(f"Mesh Shape (dp,mp): {(mesh.shape['dp'], mesh.shape['mp'])}.")
+
+
+    if not args.resume:
+        if cfg.data.bucket_path is not None:
+            # clear bucket
+            client = storage.Client()
+            if jax.process_index() == 0:
+                bucket = storage.Bucket(client, f"{cfg.data.bucket_path}")
+                blobs = bucket.list_blobs(
+                    prefix=f"{cfg.data.checkpoint_directory}/optimizer"
+                )
+                for blob in blobs:
+                    blob.delete()
+
+                blobs = bucket.list_blobs(
+                    prefix=f"{cfg.data.checkpoint_directory}/params"
+                )
+                for blob in blobs:
+                    blob.delete()
+    
+    # TODO: Update
+    local_batch_size = cfg.training.batch_size // (jax.local_device_count())
+
+    total_tokens = num_host * (
+        cfg.training.batch_size
+        * compute_tokens_seen(
+            cfg.training.total_steps,
+            max_context=cfg.data.max_context,
+        )
+    )
+
+    if jax.process_index() == 0:
+        id = wandb.util.generate_id()
+        wandb.init(id=id, resume="allow", project=cfg.data.wandb_project)
+        flat_dict = flatten_dict(cfg)
+
+        for key in model_config.keys():
+            flat_dict[f"model.{key}"] = model_config[key]
+
+        flat_dict["training.local_batch_size"] = local_batch_size
+        flat_dict["runtime"] = platform
+        flat_dict["Total Training Tokens"] = total_tokens / 1e9
+        flat_dict["Total Devices"] = num_devices
+        flat_dict["mesh (mp)"] = mesh.shape['mp']
+        flat_dict["mesh (dp)"] = mesh.shape['dp']
+        wandb.config.update(flat_dict)
+
+    def preprocess(batch):
+        x = batch["input_id.pth"][: cfg.data.max_context]
+        if type(x) == torch.tensor:
+            return jnp.array(x.long(), dtype=jnp.int32)
+        else:
+            return jnp.array(x, dtype=jnp.int32)
+
+    from itertools import islice
+
+    def split_by_jax_process(src):
+        host_id, num_process = (
+            jax.process_index(),
+            num_host,
+        )
+        if num_process > 1:
+            for s in islice(src, host_id, None, num_process):
+                yield s
+        else:
+            for s in src:
+                yield s
+
+    train_dataset = wds.DataPipeline(
+        wds.SimpleShardList(train_shards),
+        split_by_jax_process,
+        wds.tarfile_to_samples(handler=wds.warn_and_continue),
+        wds.shuffle(1e7, initial=1e7, rng=pyrandom.Random(23 + resume_step)),
+        wds.decode(handler=wds.warn_and_continue),
+        wds.map(preprocess),
+    ).repeat(nepochs=cfg.training.max_epochs)
+
+    validation_dataset = wds.DataPipeline(
+        wds.SimpleShardList(validation_shards),
+        split_by_jax_process,
+        wds.tarfile_to_samples(handler=wds.warn_and_continue),
+        wds.shuffle(1e6, initial=1e6, rng=pyrandom.Random(23 + resume_step)),
+        wds.decode(handler=wds.warn_and_continue),
+        wds.map(preprocess),
+    )
+
+    tl = DataLoader(
+        dataset=train_dataset,
+        batch_size=cfg.training.batch_size,
+        collate_fn=numpy_collate,
+        drop_last=True,
+        num_workers=0,
+    )
+
+    vl = DataLoader(
+        dataset=validation_dataset,
+        batch_size=cfg.training.batch_size // 4,
+        collate_fn=numpy_collate,
+        drop_last=True,
+        num_workers=0,
+    )
+
+    running_metrics = []
+
+    if cfg.training.train_context < cfg.data.max_context:
+        seq_len = cfg.training.train_context
+    else:
+        seq_len = cfg.data.max_context
+
+    accum_steps = cfg.training.gradient_accumulation_steps
+
+    rng = jax.random.fold_in(rng, resume_step)  # fold in resume step to create new rng
+
+    # quick way to track global step count when resuming a run
+    new_steps = 0
+
+    iterator_resume_step = int(resume_step % cfg.data.steps_per_epoch)
+    
+    for i, batch in enumerate(tqdm(tl, disable=not jax.process_index() == 0)):
+
+        if (resume_step + new_steps) > cfg.training.total_steps:
+            if jax.process_index() == 0:
+                logger.debug(f"Training has completed.")
+
+            return True
+
+        if i < iterator_resume_step:
+            continue
+
+        rng, dropout_rng = jax.random.split(rng, 2)
+
+        if seq_len < cfg.data.max_context:
+            batch = batch.reshape(-1, seq_len)
+
+        t0 = time()
+        grads, metrics = train_step_tp(params, batch)
+
+        with mesh:
+            params, opt_state = update_opt_step_tp(grads, opt_state, params)
+
+        metrics["Train Sequence Length"] = seq_len
+        metrics["Learning Rate"] = learning_rate_fn(resume_step + new_steps)
+        t1 = time()
+        metrics["Step Time (s)"] = t1 - t0
+
+        running_metrics.append(metrics)
+
+        train_metrics_np = {
+            k: np.mean([metrics[k] for metrics in running_metrics])
+            for k in running_metrics[0]
+        }
+
+        running_metrics = []
+        validation_metrics = []
+
+        absolute_step = resume_step + new_steps
+
+        train_metrics_np["Tokens Seen (B)"] = (
+            num_host
+            * (
+                cfg.training.batch_size
+                * compute_tokens_seen(
+                    absolute_step,
+                    max_context=cfg.data.max_context,
+                )
+            )
+            / 1e9
+        )
+
+        new_steps += 1
+
+        # TODO: Update
+        if (i) % (cfg.training.evaluation_frequency) == 0:
+            for val_it, val_batch in enumerate(
+                tqdm(vl, disable=not jax.process_index() == 0)
+            ):
+                val_batch = val_batch.reshape(-1, seq_len)
+                val_batch = val_batch.reshape(
+                    jax.device_count(),
+                    val_batch.shape[0] // (jax.device_count()),
+                    seq_len,
+                )
+                if val_it < cfg.training.maximum_evaluation_steps:
+                    metrics = eval_step_tp(params, val_batch)
+                    validation_metrics.append(metrics)
+                else:
+                    break
+
+            validation_metrics_np = {
+                k: np.mean([metrics[k] for metrics in validation_metrics])
+                for k in validation_metrics[0]
+            }
+
+
+            if jax.process_index() == 0:
+
+                train_metrics_np.update(validation_metrics_np)
+                wandb.log(train_metrics_np)
+
+                if save_to_bucket:
+                    save_checkpoint(
+                        params,
+                        opt_state,
+                        absolute_step,
+                        workdir=f"gs://{cfg.data.bucket_path}/{cfg.data.checkpoint_directory}/params",
+                    )
+
+        else:
+            if jax.process_index() == 0:
+                wandb.log(train_metrics_np)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/training/train_functions.py
@@ -0,0 +1,103 @@
+"""  
+Move these to a separate function temporarily
+"""
+
+from typing import Any
+
+import jax
+import jax.numpy as jnp
+import optax
+from jax.lax import with_sharding_constraint
+
+
+def to_bf16(t):
+    return jax.tree_map(
+        lambda x: x.astype(jnp.bfloat16) if x.dtype == jnp.float32 else x, t
+    )
+
+
+def to_f32(t):
+    return jax.tree_map(
+        lambda x: x.astype(jnp.float32) if x.dtype == jnp.bfloat16 else x, t
+    )
+
+
+def train_step(
+    params: Any,
+    batch: jnp.array,
+    accum_steps: int = 8,
+    model: Any = None,
+):
+    """
+    Computes loss/grads for a single batch of data, optionally with gradient accumulation
+    """
+    _, context = batch.shape
+
+    # reshape to add a microbatch dimension
+    batch = batch.reshape(accum_steps, -1, context)
+    params = to_bf16(params)
+
+    def loss_fn(params, batch):
+        _, loss = model.apply(
+            {"params": params["params"]},
+            x=batch,
+            labels=batch,
+            train=False,
+        )
+        return jnp.mean(loss)
+
+    grad_fn = jax.value_and_grad(loss_fn)
+
+    # accumulate gradients
+    def cumul_minibatch_step(carry, x_y):
+        cumul_loss, cumul_grads = carry
+        minibatch = x_y
+        loss, grads = grad_fn(params, minibatch)
+        cumul_grads = jax.tree_map(jnp.add, cumul_grads, grads)
+        return (cumul_loss + loss, cumul_grads), None
+
+    grad_init = to_bf16(jax.tree_util.tree_map(jnp.zeros_like, params))
+
+    with jax.named_scope("scanned_microbatch"):
+        (loss, grads), _ = jax.lax.scan(
+            cumul_minibatch_step, init=(jnp.zeros(()), grad_init), xs=batch
+        )
+
+    with jax.named_scope("gradient_all_reduce"):
+        grads = jax.lax.pmean(grads, axis_name="dp")
+
+    loss, grads = jax.tree_util.tree_map(lambda x: x / accum_steps, (loss, grads))
+
+    metrics = {
+        "train/loss": loss,
+        "train/ppl": jnp.exp(loss),
+    }
+
+    return grads, metrics
+
+def eval_step(
+    params: Any,
+    batch: jnp.array,
+    model: Any,
+):
+    params = to_bf16(params)
+    _, loss = model.apply(
+        {"params": params["params"]}, x=batch, labels=batch, train=False
+    )
+
+    loss = jax.lax.pmean(loss, axis_name="batch")
+
+    metrics = {"validation/loss": loss, "validation/ppl": jnp.exp(loss)}
+
+    return metrics
+
+
+def update_opt_state(
+    params: Any, grads: Any, opt_state: Any, optimizer: Any, tp_spec: Any
+):
+    # updates the optimizer state and params
+    params = with_sharding_constraint(params, tp_spec)
+    grads = with_sharding_constraint(grads, tp_spec)
+    updates, new_opt_state = optimizer.update(grads, opt_state, params)
+    new_params = optax.apply_updates(params, updates)
+    return new_params, new_opt_state
