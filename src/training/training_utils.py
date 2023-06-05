453ef9a0c8b86ef08dea046985d75334005d4250
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-06-05 10:22:25 +0100

    remove unused

--- src/training/training_utils.py
+++ src/training/training_utils.py
@@ -8,27 +8,5 @@ import jax
 import jax.numpy as jnp
 from jax import random
 
-
-def initialized(key: random.PRNGKey, model: nn.Module, input_shape: Tuple[int, int]):
-    """Initializes param dict for a model
-    Args:
-        key (_type_): _description_
-        image_size (_type_): _description_
-        model (_type_): _description_
-    Returns:
-        _type_: _description_
-    """
-
-    init_batch = jnp.ones((input_shape), dtype=jnp.int32)
-
-    def init(rng, init_batch):
-        return model.init(rng, init_batch, None, False)
-
-    jit_apply = jax.jit(init, backend="cpu")
-    variables = jit_apply(rng=key, init_batch=init_batch)
-    return variables
-
-
 def compute_tokens_seen(absolute_step, max_context):
-
     return absolute_step * max_context
